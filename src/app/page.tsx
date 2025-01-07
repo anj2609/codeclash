53455d11f719ad247dea36bcec5311f9170f1310
anj2609 <anjaligupta25092005@gmail.com>
2025-01-07 13:34:23 +0530

    Enhance landing page layout and content; update heading styles, add welcome message, and implement 'Get Started' button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import LabelButton from "@/components/ui/LabelButton";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-extrabold flex justify-center items-center h-screen">  
-        Landing page
+      <h1 className="text-4xl font-extrabold flex flex-col gap-12 justify-center items-center h-screen bg-[#10141D]">  
+        <h1 className='text-lg xs:text-xl sm:text-2xl lg:text-3xl font-bold text-white text-left mt-2'>
+          Welcome to CodeClash!
+        </h1>
+        <LabelButton variant="outlined" className="w-full sm:w-auto">
+          <Link href={"/get-started"}>
+            Get Started
+          </Link>  
+        </LabelButton>
       </h1>
     </div>
   );
