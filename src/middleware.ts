ed074a03c030c7356ea5c3e098dbd81091fe57ed
anj2609 <anjaligupta25092005@gmail.com>
2025-04-27 15:21:03 +0530

    reverted the battle pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,7 +60,7 @@ export function middleware(request: NextRequest) {
   ];
   if (publicRoutes.includes(pathname)) {
     if (accessToken) {
-      return NextResponse.redirect(new URL("/contest/join", request.url));
+      return NextResponse.redirect(new URL("/dashboard", request.url));
     }
     return NextResponse.next();
   }
